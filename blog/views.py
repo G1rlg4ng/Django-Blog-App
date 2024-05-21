c561164b15465d76409581360bbb1e9419f8441c
G1rlg4ng <kariukifaith741@gmail.com>
2024-05-21 13:07:45 +0300

    Add alternative post list view

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
+from django.views.generic import ListView
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
-
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)  # Pagination with three posts per page
