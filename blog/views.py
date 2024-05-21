4cc418e3ef9d469575e5c9f98fe711bd45664ce7
G1rlg4ng <kariukifaith741@gmail.com>
2024-05-21 17:54:02 +0300

    Add share posts by email

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import ListView
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
+from django.core.mail import send_mail
 from .models import Post
+from .forms import EmailPostForm
+
 
 class PostListView(ListView):
     """
@@ -11,6 +14,8 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)  # Pagination with three posts per page
@@ -37,4 +42,37 @@ def post_detail(request, year, month, day, post):
     )
     return render(request, 'blog/post/detail.html', {
         'post': post
+    })
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (
+                f"{cd['name']} ({cd['email']})"
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {
+        'post': post,
+        'form': form,
+        'sent': sent
     })
